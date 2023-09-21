38d7dd1d8fe4748bf0e69163ac8059eb6781f103
Jose Luis Sumba <jluis.sumba@gmail.com>
2023-09-21 11:25:23 -0500

    Add body-parser to read body as json at request

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const axios = require('axios')
 const app = express()
 const port = 3000
+
+// create application/json parser
+app.use(bodyParser.json())
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const urlUsers = 'https://jsonplaceholder.typicode.com/users';
 
 app.get('/', async (req, res) => {
   const { data } = await axios.get(urlUsers);
   res.send(data);
+})
+
+app.post('/', async (req, res) => {
+   console.log(req.body)
+   res.sendStatus(200);
 })
 
 app.listen(port, () => {
