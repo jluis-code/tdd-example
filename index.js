999e635560ba4732b3001abcdfc9a0443cf9d886
Jose Luis Sumba <jluis.sumba@gmail.com>
2023-09-21 10:51:23 -0500

    Add axios to consume external api

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
 const port = 3000
 
+const urlUsers = 'https://jsonplaceholder.typicode.com/users';
+
 app.get('/', async (req, res) => {
-  res.send('Hello World!')
+  const { data } = await axios.get(urlUsers);
+  res.send(data);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
