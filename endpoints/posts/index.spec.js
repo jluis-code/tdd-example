4c2fba158acdb25f88ca7706b1edca4237594fbf
Jose Luis Sumba <jluis.sumba@gmail.com>
2023-09-25 10:55:13 -0500

    Unit test to Posts

--- endpoints/posts/index.spec.js
+++ endpoints/posts/index.spec.js
@@ -1,7 +1,90 @@
+//https://jsonplaceholder.typicode.com/posts
+const postsHanndler = require('./index')
+
 describe('Endpoints', () => {
     describe('posts', () => {
-        it.skip('should create', () => {
+        it('should create', async () => {
+
+            const mockUsers = [
+                { "id": 1 },
+                { "id": 2 },
+            ] 
             
+            const post = {
+                userId: 1,
+                title: 'title',
+                body: 'Cuerpo del post'
+            }
+
+            const req = {
+                body: post
+            }
+
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                send: jest.fn()
+            }
+
+            const axios = {
+                get: jest.fn().mockResolvedValue({data: mockUsers}),
+                post: jest.fn().mockResolvedValue({data: {id: 1000}})
+            }
+
+            await postsHanndler({ axios }).post(req, res);
+            expect(res.status.mock.calls).toEqual([
+                [201]
+            ])
+
+            expect(res.send.mock.calls).toEqual([
+               [ {id: 1000}]
+            ])
+
+            expect(axios.post.mock.calls).toEqual([
+                ['https://jsonplaceholder.typicode.com/posts', post]
+            ])
+
+            expect(axios.get.mock.calls).toEqual([
+                ['https://jsonplaceholder.typicode.com/users']
+            ])
+
+
+
+        })
+
+        it('should not create if user id does not exist', async () => {
+
+            const mockUsers = [
+                { "id": 1 },
+                { "id": 2 },
+            ] 
+            
+            const post = {
+                userId: 3,
+                title: 'title',
+                body: 'Cuerpo del post'
+            }
+
+            const req = {
+                body: post
+            }
+
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                send: jest.fn(),
+                sendStatus : jest.fn()
+            }
+
+            const axios = {
+                get: jest.fn().mockResolvedValue({data: mockUsers}),
+                post: jest.fn().mockResolvedValue({data: {id: 1000}})
+            }
+
+            await postsHanndler({ axios }).post(req, res);
+
+            expect(axios.post.mock.calls).toEqual([])
+            expect(res.sendStatus.mock.calls).toEqual([
+                [500]
+            ])
         })
     })
 })
