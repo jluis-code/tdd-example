9dbc23c2223057c64b0a3ce96f35bb3c8488b68a
Jose Luis Sumba <jluis.sumba@gmail.com>
2023-09-25 10:00:48 -0500

    Unit tes to to middleware

--- server.js
+++ server.js
@@ -2,18 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const axios = require('axios')
 const app = express()
-const { users } = require('./endpoints')
+const { posts } = require('./endpoints')
+const { authenticate } = require('./middlewares')
 const port = 3000
 
 // create application/json parser
 app.use(bodyParser.json())
 
-const usersHanndler = users({axios})
+const postsHanndler = users({axios})
 
-app.get('/', usersHanndler.get)
-app.post('/', usersHanndler.post)
-app.put('/:id', usersHanndler.put)
-app.delete('/:id', usersHanndler.delete)
+app.post('/', authenticate, postsHanndler.post)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
